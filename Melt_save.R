01628918ddacd01e0b582da863e235ddcbabe911
Clanren <clanren@me.com>
2021-05-15 22:15:37 +1000

    Fine tuning

--- Melt_save.R
+++ Melt_save.R
@@ -2,7 +2,7 @@
 Covid_data_file <- merge(Americas, Europe, by = 'Date') 
 tail(Covid_data_file)
 Covid_data_file<- Covid_data_file %>% right_join(Asia, by = 'Date') %>% right_join(Africa, by = 'Date') %>% right_join(Oceania, by = 'Date') %>% right_join(Total, by = 'Date')
-tail(Covid_data_file)
+tail(Covid_data_file) 
 
 #save data file for inspection/ use elsewhere and then melt for charts
 write.csv(Covid_data_file, "data/Covid-Case-Count-by-Continent.csv", row.names = FALSE)
